385823345074b01c6a3efd21ce4644592eab08d1
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-10 12:32:06 +0530

    get the hold of all particular HTML elements to manipulate...

---
+++ script.js
@@ -0,0 +1,4 @@
+const cupSmall = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
