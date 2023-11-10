cba43b596232d4d32c543b7a8f8b034b5ea2fda1
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-10 13:42:42 +0530

    Added some more scripts for adding dynamic big cup animation...

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
 smallcups.forEach((cup, idx) => {
-  cup.addEventListener("click", () => highLightCups(idx));
+  cup.addEventListener("click", () => highLightSmallCups(idx));
 });
 
-function highLightCups(idx) {
+function highLightSmallCups(idx) {
   if (
     smallcups[idx].classList.contains("full") &&
     !smallcups[idx].nextElementSibling.classList.contains("full")
@@ -22,4 +22,27 @@ function highLightCups(idx) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCups();
+}
+
+function updateBigCups() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallcups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = "0";
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = "0";
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
