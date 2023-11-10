6dc98c13dcadf611aa6b0ef2ade901fa9048e273
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-10 12:50:40 +0530

    Added the scripts for adding the click animation on small cups...

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const cupSmall = document.querySelectorAll(".cup-small");
+const smallcups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+smallcups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highLightCups(idx));
+});
+
+function highLightCups(idx) {
+  smallcups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
